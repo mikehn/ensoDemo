915827dd84220aa13e0501d62515f8ff538ccba7
mike <mike.hsn.work@gmail.com>
2024-04-02 20:57:16 +0300

    dark mode update 5

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from 'views/App'
 import './main.style.css'
-
+localStorage.theme = 'light'
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@
 import colors from 'tailwindcss/colors'
 
 export default {
-  darkMode: 'false',
+  darkMode: 'class',
   content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
   theme: {
     colors: {
